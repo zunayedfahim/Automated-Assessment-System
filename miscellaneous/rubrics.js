c65edd94fb18daf290276f9a52fff5b280160f7e
Zunayed Fahim <fahimzunayed@gmail.com>
2024-03-04 15:07:49 +0600

    miscellaneous-addedd

---
+++ miscellaneous/rubrics.js
@@ -0,0 +1,32 @@
+const rubrics = [
+  {
+    question: "Questions 1",
+    levels: [
+      {
+        id: 1,
+        marks: 0,
+        sampleAnswer: "Sample Answer",
+      },
+      {
+        id: 2,
+        marks: 5,
+        sampleAnswer: "Sample Answer",
+      },
+    ],
+  },
+  {
+    question: "Questions 2",
+    levels: [
+      {
+        id: 1,
+        marks: 0,
+        sampleAnswer: "Sample Answer",
+      },
+      {
+        id: 2,
+        marks: 5,
+        sampleAnswer: "Sample Answer",
+      },
+    ],
+  },
+];
