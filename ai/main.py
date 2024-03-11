4042371db342384a4dce5a06582be6807037ba3f
Zunayed Fahim <fahimzunayed@gmail.com>
2024-03-11 13:12:38 +0600

    final-update

--- ai/main.py
+++ ai/main.py
@@ -2,9 +2,16 @@ from typing import Union
 from roberta import roberta_model
 from mistral import mistral_model
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    question: Union[str, None] = None
+    answer: Union[str, None] = None
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return mistral_model()
+
+@app.post("/")
+async def read_root(item: Item):
+    return mistral_model(item.question, item.answer)
--- ai/mistral.py
+++ ai/mistral.py
@@ -6,36 +6,26 @@ from ctransformers import AutoModelForCausalLM
 metrics = load('bertscore')
 
 
-def mistral_model():
+def mistral_model(question, answer):
 
     # Set gpu_layers to the number of layers to offload to GPU. Set to 0 if no GPU acceleration is available on your system.
     llm = AutoModelForCausalLM.from_pretrained("TheBloke/Mistral-7B-v0.1-GGUF", model_file="mistral-7b-v0.1.Q4_K_M.gguf", model_type="mistral", gpu_layers=0)
 
-    # prompt = "What is Process Control in Industrial Automation?"
-    question = 'How do arteries differ from veins?'
-    answer = "Arteries carry oxygen-rich blood away from the heart, while veins return deoxygenated blood. Arteries have thicker, muscular walls to handle high pressure, while veins have thinner walls and valves to prevent backflow."
-    # mistral_prompt = f"<s>[INST] {prompt} [/INST]"
+    # mistral_prompt = f"<s>[INST] {question} [/INST]"
+    # mistral_prompt = f"<s>[INST] You're given a question. The highest marks for the given question is 4. Now just answer the question only. [/INST] question: {question}"
+    # mistral_prompt = f"<s>[INST] Question: {question} [/INST]</s>[INST] Answer the question in 4-5 sentences. [/INST]"
+    # mistral_prompt = f"<s>Question: {question} Answer: {answer}</s>[INST] You're given a question and an answer. The answer is categorized as level 3 which contain 4 marks. Now answer the question in 4-5 sentences. [/INST]"
+    # mistral_prompt = f"<s>{question}</s>[INST] Answer the given question as if you are a student. [/INST]"
+    # mistral_prompt = f"[INST] Suppose you're an examiner. Answer the given question according to your expectation from your students. [/INST] <s>Question: {question}</s>"
+    # mistral_prompt = f"<s>[INST] Question: {question} [/INST]"
+    mistral_prompt = f"<s> Question: {question} Context: {answer}</s> [INST] Answer the given question according to the given context. [/INST]"
 
-    res = llm(question)
+    res = llm(mistral_prompt)
     # print(answer)
 
     accuracy_score = metrics.compute(references=[answer], predictions=[res], lang="en")
 
-    print(accuracy_score)
-
-
-
-
-    # model_name = "TheBloke/Mistral-7B-Instruct-v0.1-GGUF"
-    # # tokenizer = AutoTokenizer.from_pretrained(model_name)
-    # model = AutoModel.from_pretrained(model_name)
-    # nlp = pipeline("text-generation", model=model)
-
-
-    # # TODO: Prompt is Dynamic
-    # prompt = "AI is going to"
-    # res = nlp(prompt)
-    # return res
+    return accuracy_score
 
 
 
