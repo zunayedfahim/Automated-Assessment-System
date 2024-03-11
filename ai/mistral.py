e8b1a99dd86d1503f79c3bc3cfe1a2411250885e
Zunayed Fahim <fahimzunayed@gmail.com>
2024-03-11 11:35:25 +0600

    evaluation-added

--- ai/mistral.py
+++ ai/mistral.py
@@ -1,21 +1,28 @@
 # Use a pipeline as a high-level helper
-from transformers import AutoModel, AutoTokenizer, pipeline
-
-
-# Use a pipeline as a high-level helper
-
-
-
+# from transformers import AutoModel, AutoTokenizer, pipeline
+from evaluate import load
 from ctransformers import AutoModelForCausalLM
 
+metrics = load('bertscore')
 
 
 def mistral_model():
 
     # Set gpu_layers to the number of layers to offload to GPU. Set to 0 if no GPU acceleration is available on your system.
-    llm = AutoModelForCausalLM.from_pretrained("TheBloke/Mistral-7B-v0.1-GGUF", model_file="mistral-7b-v0.1.Q4_K_M.gguf", model_type="mistral", gpu_layers=50)
+    llm = AutoModelForCausalLM.from_pretrained("TheBloke/Mistral-7B-v0.1-GGUF", model_file="mistral-7b-v0.1.Q4_K_M.gguf", model_type="mistral", gpu_layers=0)
 
-    return (llm("AI is going to"))
+    # prompt = "What is Process Control in Industrial Automation?"
+    question = 'How do arteries differ from veins?'
+    answer = "Arteries carry oxygen-rich blood away from the heart, while veins return deoxygenated blood. Arteries have thicker, muscular walls to handle high pressure, while veins have thinner walls and valves to prevent backflow."
+    # mistral_prompt = f"<s>[INST] {prompt} [/INST]"
+
+    res = llm(question)
+    # print(answer)
+
+    accuracy_score = metrics.compute(references=[answer], predictions=[res], lang="en")
+
+    print(accuracy_score)
+
 
 
 
